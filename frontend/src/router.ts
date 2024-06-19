88bcb3461387c0f241d7847b495692910f692937
VincentS <vincent.schultz.fr@gmail.com>
2024-06-19 08:53:17 +0200

    1ier commit code

---
+++ frontend/src/router.ts
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import { isAuthenticatedGuard, isNotAuthenticatedGuard } from './shared/guards';
+import { useUser } from './shared/stores';
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/TheHome.vue'),
+    },
+    {
+      path: '/connexion',
+      beforeEnter: [isNotAuthenticatedGuard],
+      component: () => import('@/views/TheLogin.vue'),
+    },
+    {
+      path: '/inscription',
+      beforeEnter: [isNotAuthenticatedGuard],
+      component: () => import('@/views/TheSignup.vue'),
+    },
+    {
+      path: '/profil',
+      beforeEnter: [isAuthenticatedGuard],
+      component: () => import('@/views/TheProfile.vue'),
+    },
+    {
+      path: '/:notfound(.*)*',
+      component: () => import('@/views/NotFound.vue'),
+    },
+  ],
+});
+
+router.beforeEach(async () => {
+  const userStore = useUser();
+  if (!userStore.loaded) {
+    await userStore.fetchCurrentUser();
+  }
+});
